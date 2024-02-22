9e06a7b89373f4378347909162e8cc8aee35f739
Лобашов Андрей <a.lobashov@yanex.ru>
2024-02-22 07:38:24 +0300

    прогламма работает

--- Program.cs
+++ Program.cs
@@ -14,29 +14,55 @@ class Program
     static void Main(string[] args)
     {
         
-        if (args.Length < 1)
+        if (args.Length < 1)// в аргументах нет ни одного элемента масива
         {
+			//задаём количество элементов
 			Console.WriteLine("введите количество элементов масива");
 			int number = Convert.ToInt32(Console.ReadLine());
 			string[] arr = new string[number];
-
+			
+			// добавляем элементы в масив
+			
 			for (int i = 0; i < number; i++)
 			{
 				Console.WriteLine($"введите элемент[{i+1}/{number}] масива");
 				arr[i] = Console.ReadLine();
 			}
+			
+			
 			args = arr;/* тут назначается ссылка
 			так как без неё автоклинер удаляет переменную арр вне скобок
 			( утечка данных )
 			проесходит ошибка (переменная не обявленна)
+			по крайнемере у меня
 			*/
 		}
 		
-        PrintArray(args);//дебаг вывод
-		Console.WriteLine($"╬	╬	╬	╬	╬	╬	╬");
+        //дебаг вывод
+		Console.WriteLine($"╬---╬	>3	╬");
+		PrintArray(args);
+		Console.WriteLine($"╬---╬		╬");//*/
+		
+		string[] new_array = new string[args.Length];
+		int new_arr_leng = 0;
+		
+		// добавление элементов в массив
+		for(int i = 0; i<args.Length; i++ ){
+			
+			if ( args[i].Length <= 3 ){
+				new_array[new_arr_leng]=args[i];
+				new_arr_leng++;//новая длинна +1
+			}
+			
+		}
+		
+		// изменяем размер масива избавляясь от пустых ячеек
+		Array.Resize(ref new_array, new_arr_leng);
 		
 		
+		Console.WriteLine("");
+		Console.WriteLine("новый массив");
+		PrintArray(new_array);		
 	}
-	
 	
 }
